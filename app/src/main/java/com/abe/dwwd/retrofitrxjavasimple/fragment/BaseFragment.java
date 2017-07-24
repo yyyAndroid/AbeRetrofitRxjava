61e05ba78ede4142eee0d5f2662abc8c53372f18
yyyAndroid <noExceptionYes@gmail.com>
2017-07-24 18:50:02 +0800

    更新

---
+++ app/src/main/java/com/abe/dwwd/retrofitrxjavasimple/fragment/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.abe.dwwd.retrofitrxjavasimple.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by abe on 2017/7/24.
+ */
+
+public class BaseFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/abe/dwwd/retrofitrxjavasimple/fragment/HomeFragment.java
@@ -0,0 +1,9 @@
+package com.abe.dwwd.retrofitrxjavasimple.fragment;
+
+
+/**
+ * Created by abe on 2017/7/24.
+ */
+
+public class HomeFragment extends BaseFragment {
+}
